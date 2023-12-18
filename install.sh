8d3c8ab8f87ebc9e36647e1d5f71186457fb7032
Lenonsart <lenonsart@gmail.com>
2023-12-18 14:52:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 
 # xorg
-sudo apt install -y xorg xinit xserver-xorg xbacklight xbindkeys xvkbd xinput fonts-noto picom
+sudo apt install -y xorg xinit xserver-xorg xbacklight xbindkeys xvkbd xinput picom
 
 # User directories
 xdg-user-dirs-update
@@ -11,7 +11,7 @@ xdg-user-dirs-update
 sudo apt install -y pipewire-audio pipewire-alsa pavucontrol playerctl
 
 # Awesome
-sudo apt install -y awesome network-manager rxvt-unicode rofi deepin-icon-theme flameshot
+sudo apt install -y awesome awesome-extra awesome-doc network-manager rofi micro
 
 # Apps
-sudo apt install -y firefox-esr gvfs thunar thunar-archive-plugin thunar-volman unzip mpv htop ristretto synaptic mousepad
+sudo apt install -y firefox-esr gvfs thunar thunar-archive-plugin thunar-volman unzip mpv htop synaptic 
